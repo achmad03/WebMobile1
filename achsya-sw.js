076b9e007bf361150602fd4bb3f04b170a694040
achmad03 <38776007+achmad03@users.noreply.github.com>
2019-04-18 16:42:46 +0700

    update
    karena foto terlalu banyak jadi hanya bisa enampilkan foto dengan resolusi layar smartphone

--- achsya-sw.js
+++ achsya-sw.js
@@ -1,16 +1,62 @@
-self.addEventListener('install', function(event) {
-    //akan dijalankan bila service worker sedang diinstall oleh sistem
-    console.log('Service worker installing...');
-    //pada bagian ini bisa ditambahkan script untuk perubahan pada serviceworker berjalan, self.skipwaiting
-    //akan menambahkan listener pada website untuk mendengar event pada fetch yang terjadi pada website. 
-    //nilai fetct yang didapat akan dimasukkan ke dalam console log
-    self.addEventListener('fetch', function(event) {
-        console.log('Fetching:', event.request.url);
-        });
-    //akan melakukan skip pada waiting service worker 
-    self.skipWaiting();
-    });
-    self.addEventListener('activate', function(event) {
-    //akan dijalankan bila service worker telah diaktifasi oleh sistem
-    console.log('Service worker activating...');
-    });
+var CACHE_NAME = 'static-cache';
+   //digunakan untuk memberikan list berupa array untuk menyimpan direktory dan file apa saja yang akan disimpan ke dalam cache browser
+   var urlsToCache = [
+   '.',
+   'index.html',
+   'bootstrap/css/styles.css',
+   'bootstrap/css/bootstrap.min.new.css',
+   'bootstrap/js/jquery.min.js',
+   'bootstrap/js/bootstrap.min.new.js',
+   'foto.html',
+   'galeri.html',
+   'images/uiUx.png',
+   'images/backEnd.png',
+   'images/frontEnd.png',
+   'images/admin.jpg',
+   'images/wireless.png'
+
+
+   ];
+   //digunakan untuk memberikan listener saat terdapat sebuah event install pada browser lalu akan menambahkan semua cache yang dituliskan sebelumnya 
+   //ke dalam cache browser
+   self.addEventListener('install', function(event) {
+   event.waitUntil(
+   caches.open(CACHE_NAME)
+   .then(function(cache) {
+   return cache.addAll(urlsToCache);
+   })
+   );
+   });
+   
+   //digunakan untuk memberikan listener saat terdapat event fetch pada browser maka akan dilakukan pengecekan apakah cache browser sesuai dengan cache dari server
+   //bila sama maka akan menampilkan yang ada di cache. lalu bila cache tidak sama maka akan mengambil cache yang ada dalam server untuk disimpan pada browser
+   //bila tidak ada koneksi internet maka akan membuka cache yang tersimpan pada cahe browser
+   self.addEventListener('fetch', function(event) {
+       event.respondWith(
+           //untuk mencocokkan dengan html request apakah cache browser sama atau tidak dengan yang ada di server
+       caches.match(event.request)
+       .then(function(response) {
+       return response || fetchAndCache(event.request);
+       })
+       );
+       });
+       function fetchAndCache(url) {
+       return fetch(url)
+       .then(function(response) {
+       // Check if we received a valid response
+       if (!response.ok) {
+       throw Error(response.statusText);
+       }
+       //digunakan untuk membuat cache baru pada browser
+       return caches.open(CACHE_NAME)
+       .then(function(cache) {
+           //akan menyalin cache yang dikirim dari server dengan susunan direktory dan file yang sesuai dengan yang ada di server
+       cache.put(url, response.clone());
+       return response;
+       });
+       })
+       .catch(function(error) {
+       console.log('Request failed:', error);
+       // You could return a custom offline 404 page here
+       });
+       }
